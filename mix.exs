a69e9a6b0d456f52fd98d5cc4239316f58f39861
Christopher Cote <chris@entropealabs.com>
2017-02-21 16:09:02 -0600

    initial commit

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule RosettaHomeChromecast.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :rosetta_home_chromecast,
+     version: "0.1.0",
+     elixir: "~> 1.4",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    # Specify extra applications you'll use from Erlang/Elixir
+    [extra_applications: [:logger, :chromecast]]
+  end
+
+  defp deps do
+    [
+      {:chromecast, "~> 0.1.2"},
+      {:mdns, "~> 0.1.5"},
+      {:rosetta_home, github: "rosetta-home/cicada", branch: "dependency"},
+    ]
+  end
+end
---
+++ test/rosetta_home_chromecast_test.exs
@@ -0,0 +1,8 @@
+defmodule RosettaHomeChromecastTest do
+  use ExUnit.Case
+  doctest RosettaHomeChromecast
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
