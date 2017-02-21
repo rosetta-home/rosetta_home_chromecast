c617d742dec6d77b5ad678e1b6ad9d99ff4b917c
Christopher Cote <chris@entropealabs.com>
2017-02-21 16:11:03 -0600

    make sure mdns is running

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule RosettaHomeChromecast.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :chromecast]]
+    [extra_applications: [:logger, :chromecast, :mdns]]
   end
 
   defp deps do
