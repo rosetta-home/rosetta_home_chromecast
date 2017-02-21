ee27346635169cb1c92f0773a28c6ba5237538b8
Christopher Cote <chris@entropealabs.com>
2017-02-21 17:19:09 -0600

    adds cicada dependencies

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule RosettaHomeChromecast.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :chromecast, :mdns]]
+    [extra_applications: [:logger, :chromecast, :mdns, :network_manager, :device_manager]]
   end
 
   defp deps do
