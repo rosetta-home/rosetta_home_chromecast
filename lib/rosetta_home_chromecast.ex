0ed348a9abf8efbb88391b54696b96d8b2511cdb
Christopher Coté <chris@entropealabs.com>
2017-03-17 15:46:19 -0500

    update for histograms

--- lib/rosetta_home_chromecast.ex
+++ lib/rosetta_home_chromecast.ex
@@ -1,5 +1,5 @@
 defmodule Cicada.DeviceManager.Device.MediaPlayer.Chromecast do
-  use GenServer
+  use Cicada.DeviceManager.DeviceHistogram
   require Logger
   alias Cicada.DeviceManager.Device.MediaPlayer
   alias Cicada.DeviceManager
